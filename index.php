2c8be90515c62032cc8dd5e52c54c7c32d9d86d4
Luis Fuentes <LDFG1002@gmail.com>
2023-03-23 16:08:02 -0500

    ejercicios de sintaxis básica de PHP: comparación y variables

--- index.php
+++ index.php
@@ -9,18 +9,51 @@
     ];
 
     // Arithmetic
-    echo "<pre>";                                          # <pre> is an HTML element that preformats the text  
-    echo "Suma 2 + 2 = "         . ((int) 2 + (int) 2);    # '.' is for concatenate strings in PHP and '(int)' defines '2' as a integer
-    echo "<br>";                                           # <br> is an HTML element that makes a jumpt to the next line
+    echo "<pre>";                                                   # <pre> is an HTML element that preformats the text
+    echo "Suma 2 + 2 = "         . ((int) 2 + (int) 2);             # '.' is for concatenate strings in PHP and '(int)' defines '2' as a integer
+    echo "<br>";                                                  # <br> is an HTML element that makes a jumpt to the next line
     echo "Resta 2 - 2 = "        . ((int) 2 - (int) 2);
     echo "<br>";
     echo "Multiplica 2 * 2 = "   . ( 2 * 2);
     echo "<br>";
     echo "Divide 2 / 2 = "       . ( 2 / 2);
     echo "<br>";
-    echo "Módulo 2 % 2 = "       . ( 2 % 2);              # '%' is for obtaining the remainder of a division.
+    echo "Módulo 2 % 2 = "       . ( 2 % 2);                        # '%' is for obtaining the remainder of a division.
     echo "<br>";
-    echo "Exponencial 2 ** 2 = " . ( 2 ** 2);             # 'a**b' = 'a^b'
+    echo "Exponencial 2 ** 2 = " . ( 2 ** 2);                       # 'a**b' = 'a^b'
     echo "<br>";
+
+    // Comparison
+    echo "Igual 9 == '9' : "        . ((int) 9 == (string) '9');    # Equal: compares only the value of the elements: 9 = 9 TRUE
+    echo "<br>";
+    echo "Idéntico 9 === '9' : "    . ((int) 9 === (string) '9');   # Identical: compares the value and the type of the elements: 9 = 9, but (int) != (string) FALSE
+    echo "<br>";
+    echo "Diferente 9 != '9' : "    . ((int) 9 != (string) '9');    # Not equal: compares the value and the type of the elements: 9 != 9 FALSE
+    echo "<br>";
+    echo "Diferente 9 <> '9' : "    . ((int) 9 <> (string) '9');    # Not equal: '!=' = '<>'
+    echo "<br>";
+    echo "No idéntico 9 !== '9' : " . ((int) 9 !== (string) '9');   # Not identical: compares the value and the type of the elements: (int) != (string) TRUE
+    echo "<br>";
+    echo "Menor que 9 < '9' : "     . ((int) 9 < (string) '9');     # Less than: compares only the value of the elements: 9 < 9 FALSE
+    echo "<br>";
+    echo "Mayor que 9 > '9' : "     . ((int) 9 > (string) '9');     # Greater than: compares only the value of the elements: 9 > 9 FALSE
+    echo "<br>";
+    echo "Menor o igual que 9 <= '9' : " . ((int) 9 <= (string) '9');       # Less than or equal to: compares only the value of the elements: 9 <= 9 TRUE
+    echo "<br>";
+    echo "Mayor o igual que 9 >= '9' : " . ((int) 9 >= (string) '9');       # Greater than or equal to: compares only the value of the elements: 9 >= 9 TRUE
+    echo "<br>";
+    echo "Nave espacial 9 <=> '9' : "    . ((int) 9 <=> (string) '9');      # Spaceship: compares only the value of the elements: 9 <=> 9 return 0
+    echo "<br>";
+    echo "Fusión de null null ?? 9 ?? '9' : " . ((null) ?? (int) 9 ?? (string) '9'); # Return the first not null element: (int) 9
+    echo "<br>";
+    
+    // Variable variable
+    $app  = 'name';     # 'app'  variable has the value: (string) 'name'
+    $name = 'Platzi';   # 'name' variable has the value: (string) 'Platzi'
+    # however, since $app = 'name' and variable $name exists (value and variable have the same name), then $app = 'name' and $$app = 'Platzi'
+
+    echo $app;
+    echo "<br>";
+    echo $$app;
     echo "</pre>";
 ?>
