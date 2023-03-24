e4d5ace239a7d1d30f85aff70a8c65838db37647
Luis Fuentes <LDFG1002@gmail.com>
2023-03-24 17:51:33 -0500

    composer.lock installed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'italomoralesf/text',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9d686051b3c02e3f79556495fc3e1b14aca01fe5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'italomoralesf/text' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9d686051b3c02e3f79556495fc3e1b14aca01fe5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
